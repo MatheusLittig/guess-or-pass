9a27d19a0281b3ae1cff87d226014cec72f02cde
MatheusLittig <littig.works@gmail.com>
2022-06-20 20:56:37 -0400

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+/** packages */
+import type { Component } from 'solid-js';
+
+/** main */
+const App: Component = () => {
+
+  /** render */
+  return (
+    <div>
+      hello world
+    </div>
+  );
+};
+
+/** exports */
+export default App;
